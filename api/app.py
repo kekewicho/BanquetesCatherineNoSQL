2035dd6a715b3f67abe013873a2d72562a31a1c7
Rubén J. Navarro <133140934+rubennight@users.noreply.github.com>
2025-05-31 01:29:49 -0600

    Se mandan cambios para los endpoints

--- api/app.py
+++ api/app.py
@@ -3,9 +3,9 @@ from routes import register_blueprints
 
 app = Flask(__name__)
 
+app.secret_key = 'LabBaseDatos2'
+
 register_blueprints(app)
-
-
 
 if __name__ == "__main__":
     app.run(debug=True)
